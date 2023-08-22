c3ad10032fe588621081f6a1a9afef0d53af8e19
Muhammad Bilal Azam <124085891+mbilalazam@users.noreply.github.com>
2023-08-22 16:00:58 -0500

    Update and rename important_links.md to important_links.sh

--- important_links.sh
+++ important_links.sh
@@ -1,10 +1,11 @@
-MiniRun3.1 FHC Files
+## MiniRun3.1 FHC Files
 https://portal.nersc.gov/project/dune/data/2x2/simulation/productions/MicroRun3.1_1E18_FHC/
 
-MiniRun3.1 RHC Files
+## MiniRun3.1 RHC Files
 https://portal.nersc.gov/project/dune/data/2x2/simulation/productions/MiniRun3_1E19_RHC/
 
-MiniRun4 RHC Files
+## MiniRun4 RHC Files
 https://github.com/DUNE/2x2_sim/wiki/MiniRun4-file-locations
 
-My Directory /dune/app/users/mazam/working_area/dunendlar_multiplicity/bilal_work/inProgress/MiniRun3.1_FHC
+## My Directory 
+cd /dune/app/users/mazam/working_area/dunendlar_multiplicity/bilal_work/inProgress/MiniRun3.1_FHC
