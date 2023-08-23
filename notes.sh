be2b4889f9401532553f490d13341ff9e6bec200
Muhammad Bilal Azam <124085891+mbilalazam@users.noreply.github.com>
2023-08-23 14:08:19 -0500

    Update notes.sh

--- notes.sh
+++ notes.sh
@@ -6,6 +6,7 @@ https://portal.nersc.gov/project/dune/data/2x2/simulation/productions/MiniRun3_1
 
 ## MiniRun4 RHC Files
 https://github.com/DUNE/2x2_sim/wiki/MiniRun4-file-locations
+cd /pnfs/dune/tape_backed/users/mkramer/prod/MiniRun4/MiniRun4_1E19_RHC/MiniRun4_1E19_RHC.flow/FLOW/
 
 ## To download multiple files from the MiniRun3.1 FHC link
 wget https://portal.nersc.gov/project/dune/data/2x2/simulation/productions/MicroRun3.1_1E18_FHC/MicroRun3.1_1E18_FHC.flow/FLOW/MicroRun3.1_1E18_FHC.flow.0000{0..11}.FLOW.h5
